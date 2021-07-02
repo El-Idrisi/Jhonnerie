d1fe8052e2c2564e1a2e989dacb8b8827148db8f
El-Idrisi <86701955+El-Idrisi@users.noreply.github.com>
2021-07-02 20:05:43 +0700

    menjalakan contact form

--- js/java.js
+++ js/java.js
@@ -1,4 +1,3 @@
-
 // $('.page-scroll').on('click', function(event){
 //     event.preventDefault();
 
@@ -13,11 +12,11 @@
 // });
 
 // Smoth Scrolling
-$(document).ready(function(){
+$(document).ready(function () {
 
     var scrollLink = $('.page-scroll');
 
-    scrollLink.click(function(e){
+    scrollLink.click(function (e) {
         e.preventDefault();
         $('body,html').animate({
             scrollTop: $(this.hash).offset().top - 50
@@ -28,23 +27,19 @@ $(document).ready(function(){
 
 // Parallax 
 
-$(window).scroll(function() {
+$(window).scroll(function () {
     var wScroll = $(this).scrollTop();
 
     $('.jumbotron img').css({
-        'transform' : 'translate(0px,'+ wScroll/4 +'%)'
+        'transform': 'translate(0px,' + wScroll / 4 + '%)'
     });
 
     $('.jumbotron h1').css({
-        'transform' : 'translate(0px,'+ wScroll/2 +'%)'
+        'transform': 'translate(0px,' + wScroll / 2 + '%)'
     });
 
     $('.jumbotron p').css({
-        'transform' : 'translate(0px,'+ wScroll/1.5 +'%)'
+        'transform': 'translate(0px,' + wScroll / 1.5 + '%)'
     });
-    
+
 });
-
-
-
-
